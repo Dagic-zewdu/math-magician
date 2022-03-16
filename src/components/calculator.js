836bef703393907dd9ff26a06112fc84d724cd43
Dagi <dagi.zewdu.dz@gmail.com>
2022-03-16 09:05:34 +0300

    Fixed styling

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -6,14 +6,16 @@ export default class Calculator extends Component {
   render() {
     return (
       <div className="container">
-        <div className="display-number">
-          <h2 className="float-right white">0</h2>
+        <div className="calc-container">
+          <div className="display-number">
+            <h2 className="float-right white">0</h2>
+          </div>
+          <RenderNumbers Items={['AC', '+/-', '%', '/']} />
+          <RenderNumbers Items={[7, 8, 9, '*']} />
+          <RenderNumbers Items={[4, 5, 6, '-']} />
+          <RenderNumbers Items={[1, 2, 3, '+']} />
+          <RenderNumbers Items={[0, '.', '=']} />
         </div>
-        <RenderNumbers Items={['AC', '+/-', '%', '/']} />
-        <RenderNumbers Items={[7, 8, 9, '*']} />
-        <RenderNumbers Items={[4, 5, 6, '-']} />
-        <RenderNumbers Items={[1, 2, 3, '+']} />
-        <RenderNumbers Items={[0, '.', '=']} />
       </div>
     );
   }
