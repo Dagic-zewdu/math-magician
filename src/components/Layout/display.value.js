8d3138bf0e0063a38c38041c03737138a383a5c1
Dagi <dagi.zewdu.dz@gmail.com>
2022-03-17 00:31:28 +0300

    disabled comments for eslint

--- src/components/Layout/display.value.js
+++ src/components/Layout/display.value.js
@@ -2,13 +2,7 @@ import React from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Display extends React.Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    // eslint-disable-next-line react/prop-types
     const { next = '', operation = '', total = null } = this.props;
     return (
       <div
