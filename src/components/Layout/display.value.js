78ba08cd6fe57df2de893b3375e60b39d93ba14c
Dagi <dagi.zewdu.dz@gmail.com>
2022-03-17 10:47:09 +0300

    Changed from class component to functional component

--- src/components/Layout/display.value.js
+++ src/components/Layout/display.value.js
@@ -1,22 +1,17 @@
 import React from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
-class Display extends React.Component {
-  render() {
-    const { next = '', operation = '', total = null } = this.props;
-    return (
-      <div
-        className="g-item gray"
-        style={{
-          gridColumn: '1/5',
-          justifyContent: 'flex-end',
-          fontSize: '4rem',
-        }}
-      >
-        {next || operation || total || '0'}
-      </div>
-    );
-  }
-}
+const Display = ({ next = '', operation = '', total = null }) => (
+  <div
+    className="g-item gray"
+    style={{
+      gridColumn: '1/5',
+      justifyContent: 'flex-end',
+      fontSize: '4rem',
+    }}
+  >
+    {next || operation || total || '0'}
+  </div>
+);
 
 export default Display;
